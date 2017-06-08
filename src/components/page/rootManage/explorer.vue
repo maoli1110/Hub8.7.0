08a41e12c3738d905decb3054d1e78320551ad9b
ppc <978648119@qq.com>
2017-06-08 16:26:50 +0800

    人员管理 角色管理整体布局

--- src/components/page/rootManage/explorer.vue
+++ src/components/page/rootManage/explorer.vue
@@ -1,90 +1,422 @@
 <template>
-    <div>
-        <el-table
-            ref="multipleTable"
-            :data="tableData3"
-            border
-            tooltip-effect="dark"
-            style="width: 100%">
-            <el-table-column
-                type="selection"
-                width="55">
-            </el-table-column>
-            <el-table-column
-                label="日期"
-                width="120">
-                <template scope="scope">{{ scope.row.date }}</template>
-            </el-table-column>
-            <el-table-column
-                prop="name"
-                label="姓名"
-                width="120">
-            </el-table-column>
-            <el-table-column
-                prop="address"
-                label="地址"
-                show-overflow-tooltip>
-            </el-table-column>
-        </el-table>
-        <div style="margin-top: 20px">
-            <el-button @click="toggleSelection([tableData3[1], tableData3[2]])">切换第二、第三行的选中状态</el-button>
-            <el-button @click="toggleSelection()">取消选择</el-button>
-        </div>
-    </div>
-</template>
-<script>
-    export default{
-        data(){
-            return {
-                menusData:[{name:"explorer",routerDump:'explorer'},{name:'质检计量',routerDump:'qualityMeasure'}],
-                tableData3: [{
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-08',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-06',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-07',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }],
-                multipleSelection: []
-            }
-        },
-        components:{
+	<section style='margin-top:20px'>
+		<!--工具条-->
+		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+			<el-form :inline="true" :model="filters">
+                <el-form-item>
+					      <el-select v-model="value" placeholder="请选择">
+                             <el-option
+                               v-for="item in options"
+                               :key="item.value"
+                               :label="item.label"
+                               :value="item.value">
+                             </el-option>
+                          </el-select>
+				</el-form-item>
+				<el-form-item>
+					<el-input v-model="filters.name" placeholder="姓名"></el-input>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" v-on:click="getUsers">查询</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleAdd">新增</el-button>
+				</el-form-item>
+			</el-form>
+		</el-col>
+        <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+			<el-form :inline="true" :model="filters">
+				<el-form-item>
+					<el-button type="primary" v-on:click="getUsers">添加人员</el-button>
+				</el-form-item>
+				<el-form-item>
+					<el-button type="primary" @click="handleAdd">批量添加</el-button>
+				</el-form-item>
+                <el-form-item>
+					<el-button type="primary" @click="handleAdd">删除人员</el-button>
+				</el-form-item>
+			</el-form>
+		</el-col>
 
-        },
-        methods: {
-            toggleSelection(rows) {
-                if (rows) {
-                    rows.forEach(row => {
-                        this.$refs.multipleTable.toggleRowSelection(row);
-                });
-                } else {
-                    this.$refs.multipleTable.clearSelection();
-                }
-            },
-            handleSelectionChange(val) {
-                this.multipleSelection = val;
-            }
-        }
-    }
+		<!--列表-->
+		<el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
+			<el-table-column type="selection" width="100">
+			</el-table-column>
+			<el-table-column type="index" width="100" label="序号">
+			</el-table-column>
+			<el-table-column prop="name" label="姓名" width="120" sortable>
+			</el-table-column>
+			<el-table-column prop="sex" label="性别" width="120" :formatter="formatSex" sortable>
+			</el-table-column>
+			<el-table-column prop="age" label="年龄" width="120" sortable>
+			</el-table-column>
+			<el-table-column prop="birth" label="生日" width="180" sortable>
+			</el-table-column>
+			<el-table-column prop="addr" label="地址" min-width="120" sortable>
+			</el-table-column>
+			<el-table-column label="操作" width="180">
+				<template scope="scope">
+					<el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+				</template>
+			</el-table-column>
+		</el-table>
+
+		<!--工具条-->
+		<el-col :span="24" class="toolbar">
+		    <el-pagination style='float:right'
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page="1"
+            :page-sizes="[100, 200, 300, 400]"
+            :page-size="100"
+            layout="total, sizes, prev, pager, next, jumper"
+            :total="400">
+            </el-pagination>
+		</el-col>
+
+		<!--编辑界面-->
+		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
+				<el-form-item label="姓名" prop="name">
+					<el-input v-model="editForm.name" auto-complete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="性别">
+					<el-radio-group v-model="editForm.sex">
+						<el-radio class="radio" :label="1">男</el-radio>
+						<el-radio class="radio" :label="0">女</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="年龄">
+					<el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
+				</el-form-item>
+				<el-form-item label="生日">
+					<el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
+				</el-form-item>
+				<el-form-item label="地址">
+					<el-input type="textarea" v-model="editForm.addr"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="editFormVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+			</div>
+		</el-dialog>
+
+		<!--新增界面-->
+		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+			<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+				<el-form-item label="姓名" prop="name">
+					<el-input v-model="addForm.name" auto-complete="off"></el-input>
+				</el-form-item>
+				<el-form-item label="性别">
+					<el-radio-group v-model="addForm.sex">
+						<el-radio class="radio" :label="1">男</el-radio>
+						<el-radio class="radio" :label="0">女</el-radio>
+					</el-radio-group>
+				</el-form-item>
+				<el-form-item label="年龄">
+					<el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
+				</el-form-item>
+				<el-form-item label="生日">
+					<el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
+				</el-form-item>
+				<el-form-item label="地址">
+					<el-input type="textarea" v-model="addForm.addr"></el-input>
+				</el-form-item>
+			</el-form>
+			<div slot="footer" class="dialog-footer">
+				<el-button @click.native="addFormVisible = false">取消</el-button>
+				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+			</div>
+		</el-dialog>
+	</section>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+                options: [{
+                            value: '选项1',
+                            label: '董事长'
+                            }, {
+                            value: '选项2',
+                            label: '项目经理'
+                            }, {
+                            value: '选项3',
+                            label: '商务经理'
+                            }, {
+                            value: '选项4',
+                            label: '生产经理'
+                            }, {
+                            value: '选项5',
+                            label: '总工'
+                            }],
+                            value: '全部',
+
+				filters: {
+					name: ''
+				},
+				users: [{
+					addr:"吉林省 白山市 临江市",
+                    age:42,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏1",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:43,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏2",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:44,
+                    birth:"1979-110-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏3",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:42,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏1",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:43,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏2",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:44,
+                    birth:"1979-110-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏3",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:42,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏1",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:43,
+                    birth:"1979-120-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏2",
+                    sex:0
+				       },
+                       {
+					addr:"吉林省 白山市 临江市",
+                    age:44,
+                    birth:"1979-110-22",
+                    id:"76CB27d3-547F-6E6D-270E-32B7ED396b32",
+                    name:"姜敏3",
+                    sex:0
+				       }             
+                ],
+				total: 0,
+				page: 1,
+				listLoading: false,
+				sels: [],//列表选中列
+
+				editFormVisible: false,//编辑界面是否显示
+				editLoading: false,
+				editFormRules: {
+					name: [
+						{ required: true, message: '请输入姓名', trigger: 'blur' }
+					]
+				},
+				//编辑界面数据
+				editForm: {
+					id: 0,
+					name: '',
+					sex: -1,
+					age: 0,
+					birth: '',
+					addr: ''
+				},
+
+				addFormVisible: false,//新增界面是否显示
+				addLoading: false,
+				addFormRules: {
+					name: [
+						{ required: true, message: '请输入姓名', trigger: 'blur' }
+					]
+				},
+				//新增界面数据
+				addForm: {
+					name: '',
+					sex: -1,
+					age: 0,
+					birth: '',
+					addr: ''
+				}
+
+			}
+		},
+		methods: {
+			//性别显示转换
+			formatSex: function (row, column) {
+				return row.sex == 1 ? '男' : row.sex == 0 ? '女' : '未知';
+			},
+			handleCurrentChange(val) {
+				this.page = val;
+				this.getUsers();
+			},
+            handleSizeChange(val) {
+				this.page = val;
+				this.getUsers();
+			},
+			//获取用户列表
+			getUsers() {
+				let para = {
+					page: this.page,
+					name: this.filters.name
+				};
+				this.listLoading = true;
+				//NProgress.start();
+				getUserListPage(para).then((res) => {
+					this.total = 86;
+					// this.users = res.data.users;
+					this.listLoading = false;
+					//NProgress.done();
+				});
+			},
+			//删除
+			handleDel: function (index, row) {
+				this.$confirm('确认删除该记录吗?', '提示', {
+					type: 'warning'
+				}).then(() => {
+					this.listLoading = true;
+					//NProgress.start();
+					let para = { id: row.id };
+					removeUser(para).then((res) => {
+						this.listLoading = false;
+						//NProgress.done();
+						this.$message({
+							message: '删除成功',
+							type: 'success'
+						});
+						this.getUsers();
+					});
+				}).catch(() => {
+
+				});
+			},
+			//显示编辑界面
+			handleEdit: function (index, row) {
+				this.editFormVisible = true;
+				this.editForm = Object.assign({}, row);
+			},
+			//显示新增界面
+			handleAdd: function () {
+				this.addFormVisible = true;
+				this.addForm = {
+					name: '',
+					sex: -1,
+					age: 0,
+					birth: '',
+					addr: ''
+				};
+			},
+			//编辑
+			editSubmit: function () {
+				this.$refs.editForm.validate((valid) => {
+					if (valid) {
+						this.$confirm('确认提交吗？', '提示', {}).then(() => {
+							this.editLoading = true;
+							//NProgress.start();
+							let para = Object.assign({}, this.editForm);
+							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+							editUser(para).then((res) => {
+								this.editLoading = false;
+								//NProgress.done();
+								this.$message({
+									message: '提交成功',
+									type: 'success'
+								});
+								this.$refs['editForm'].resetFields();
+								this.editFormVisible = false;
+								this.getUsers();
+							});
+						});
+					}
+				});
+			},
+			//新增
+			addSubmit: function () {
+				this.$refs.addForm.validate((valid) => {
+					if (valid) {
+						this.$confirm('确认提交吗？', '提示', {}).then(() => {
+							this.addLoading = true;
+							//NProgress.start();
+							let para = Object.assign({}, this.addForm);
+							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
+							addUser(para).then((res) => {
+								this.addLoading = false;
+								//NProgress.done();
+								this.$message({
+									message: '提交成功',
+									type: 'success'
+								});
+								this.$refs['addForm'].resetFields();
+								this.addFormVisible = false;
+								this.getUsers();
+							});
+						});
+					}
+				});
+			},
+			selsChange: function (sels) {
+				this.sels = sels;
+			},
+			//批量删除
+			batchRemove: function () {
+				var ids = this.sels.map(item => item.id).toString();
+				this.$confirm('确认删除选中记录吗？', '提示', {
+					type: 'warning'
+				}).then(() => {
+					this.listLoading = true;
+					//NProgress.start();
+					let para = { ids: ids };
+					batchRemoveUser(para).then((res) => {
+						this.listLoading = false;
+						//NProgress.done();
+						this.$message({
+							message: '删除成功',
+							type: 'success'
+						});
+						this.getUsers();
+					});
+				}).catch(() => {
+
+				});
+			}
+		},
+		mounted() {
+			// this.getUsers();
+		}
+	}
+
 </script>
+
+<style scoped>
+
+</style>
