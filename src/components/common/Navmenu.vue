92b25acebef8027189db7ed537acd7f4075788e4
maoli1110 <848631812@qq.com>
2017-06-08 14:11:24 +0800

    总体布局更新

--- src/components/common/Navmenu.vue
+++ src/components/common/Navmenu.vue
@@ -1,6 +1,6 @@
 <template>
     <el-menu theme=""  class="el-menu-demo" mode="horizontal" router>
-        <el-menu-item index="compangyProfile">企业概况</el-menu-item>
+        <el-menu-item index="companyProfile">企业概况</el-menu-item>
         <el-menu-item index="stuffManage">权限管理</el-menu-item>
         <el-menu-item index="bimStore">BIM库</el-menu-item>
         <el-menu-item index="explorer">应用配置</el-menu-item>
