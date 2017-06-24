6dd1866f1078d0be69bc8ec72949762cf94b0d89
small-samll-girl <1441849713@qq.com>
2017-06-24 11:27:28 +0800

    data for port

--- src/api/getData.js
+++ src/api/getData.js
@@ -5,13 +5,12 @@
 import axios from 'axios';
 
 
-let base = 'http://192.168.13.215:8080/enterprise';
+let base = 'http://192.168.13.215:8082/enterprise';
 
 
 export const getUserListPage = params => axios.post(`${base}/rs/lbconfig/process/getProcessList`, params);
 
-//let base = 'http://172.16.21.240:8080';
-export const getUserListPage = params => axios.post(`${base}/enterprise/rs/lbconfig/process/getProcessList`, params);
+
 
 /*
 * 流程设置列表
@@ -46,11 +45,5 @@ export const getProjModelNodeForms = params => axios.post(`${base}/rs/lbconfig/p
 /*更新工程模板的表单*/
 export const updateProjModel = params => axios.get(`${base}/rs/lbconfig/projmodel/updateProjModel`, params);
 
-export const getLDProjModelList = params => axios.get(`${base}/enterprise/rs/lbconfig/projmodel/getLDProjModelList`, { params: params });
-/*获取工程模板展示明细*/
-export const getProjModelDetail = params => axios.get(`${base}/enterprise/rs/lbconfig/projmodel/getProjModelDetail`, { params: params });
-/*获取工程模板下某个节点关联的表单*/
-export const getProjModelNodeForms = params => axios.get(`${base}/enterprise/rs/lbconfig/projmodel/getFormInfos`, { params: params });
-/*更新工程模板的表单*/
-export const updateProjModel = params => axios.post(`${base}/enterprise/rs/lbconfig/projmodel/updateProjModel`, params);
+
 
