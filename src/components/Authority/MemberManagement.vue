f40a66a783dd0d0c00bb07b56b4d466b40570461
ppc <978648119@qq.com>
2017-11-08 17:03:51 +0800

    成员管理整体页面+弹框

--- src/components/Authority/MemberManagement.vue
+++ src/components/Authority/MemberManagement.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div class="header">
-            <div class="el-form-item">
+            <div class="el-form-item el-form_">
                 <label class="el-form-item__label" style="width: 85px;">组织节点：</label>
                 <div class="el-form-item__content" style="margin-left: 85px;">
                     <el-select v-model="orgValue" placeholder="请选择">
@@ -12,7 +12,7 @@
                     </el-select>
                 </div>
             </div>
-            <div class="el-form-item">
+            <div class="el-form-item el-form_">
                 <label class="el-form-item__label" style="width: 85px;">角色：</label>
                 <div class="el-form-item__content" style="margin-left: 85px;">
                     <el-select v-model="role" placeholder="请选择">
@@ -25,7 +25,7 @@
                     </el-select>
                 </div>
             </div>
-            <div class="el-form-item">
+            <div class="el-form-item el-form_">
                 <label class="el-form-item__label" style="width: 80px;"></label>
                 <div class="el-form-item__content" style="margin-left: 80px;">
                     <el-input placeholder="请选择日期" icon="search"></el-input>
@@ -34,9 +34,9 @@
         </div>
         <div class="main">
             <div>
-                <el-button type="success">添加人员</el-button>
-                <el-button type="warning">批量添加</el-button>
-                <el-button type="danger"> 删除人员</el-button>
+                <el-button type="success" @click="addUser()">添加人员</el-button>
+                <el-button type="warning" @click='batchAddUser()'>批量添加</el-button>
+                <el-button type="danger" @click="deleteUser()"> 删除人员</el-button>
             </div>
             <el-table ref="multipleTable" :data="memberTableData" border tooltip-effect="dark"
                       style="width: 100%;margin-top:20px" @selection-change="handleSelectionChange">
@@ -44,50 +44,173 @@
                 <el-table-column type='index' label="序号" width="55"></el-table-column>
                 <el-table-column prop="name" label="姓名" width="100"></el-table-column>
                 <el-table-column label="通行证" width="150">
-                  <template slot-scope="scope">
-                     <div :title="scope.row.pass" class="textcell" >
-                       {{ scope.row.pass }}
-                     </div>
+                    <template slot-scope="scope">
+                        <div :title="scope.row.pass" class="textcell">
+                            {{ scope.row.pass }}
+                        </div>
                     </template>
                 </el-table-column>
                 <el-table-column prop="role" label="角色" width="100"></el-table-column>
-                <el-table-column prop="phone" label="手机号码" width="180"></el-table-column>               
-                <el-table-column prop="email" label="邮箱" width="180"></el-table-column>               
+                <el-table-column prop="phone" label="手机号码" width="180"></el-table-column>
+                <el-table-column prop="email" label="邮箱" width="180"></el-table-column>
                 <el-table-column label="更新时间" width="180">
                     <template slot-scope="scope">{{ scope.row.date }}</template>
-                </el-table-column>            
-                <el-table-column prop="WAU" label="周活跃度" width="100" ></el-table-column>               
-                <el-table-column label="备注" width="200">
-                   <template slot-scope="scope">
-                     <div :title="scope.row.remarks" class="textcell">
-                       {{ scope.row.remarks }}
-                     </div>
+                </el-table-column>
+                <el-table-column  label="周活跃度" width="100">
+                    <template slot-scope="scope">
+                      <el-button type="text" @click="weeklyactivityDialogVisible=true"> {{ scope.row.WAU }}</el-button>
                     </template>
-                </el-table-column>               
-                <el-table-column label="操作" ></el-table-column>               
+                </el-table-column>
+                <el-table-column label="备注" width="200">
+                    <template slot-scope="scope">
+                        <div :title="scope.row.remarks" class="textcell">
+                            {{ scope.row.remarks }}
+
+
+                        </div>
+                    </template>
+                </el-table-column>
+                <el-table-column label="操作">
+                    <template slot-scope="scope">
+                        <span type="primary" class="el-icon-edit"></span>
+                        <span type="primary" class="el-icon-document"></span>
+                        <span type="primary" class="el-icon-delete"></span>
+                        <span type="primary" class="el-icon-search"></span>
+                    </template>
+                </el-table-column>
             </el-table>
             <div style="margin-top: 20px">
                 <div style="float:left;height:40px;line-height:40px">共10个结果</div>
-                <el-pagination  style="margin-left:30%"
-                    @size-change="handleSizeChange"
-                    @current-change="handleCurrentChange"
-                    :current-page="4"
-                    :page-sizes="[100, 200, 300, 400]"
-                    :page-size="100"
-                    layout="total, sizes, prev, pager, next, jumper"
-                    :total="400">
+                <el-pagination style="margin-left:30%"
+                               @size-change="handleSizeChange"
+                               @current-change="handleCurrentChange"
+                               :current-page="4"
+                               :page-sizes="[100, 200, 300, 400]"
+                               :page-size="100"
+                               layout="total, sizes, prev, pager, next, jumper"
+                               :total="400">
                 </el-pagination>
             </div>
 
         </div>
+        <!-- 添加人员 -->
+        <el-dialog
+            title="添加人员"
+            :visible.sync="addUserDialogVisible"
+            size="tiny">
+            <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+                <el-form-item label="鲁班通行证" prop="pass">
+                    <el-input  v-model="ruleForm.pass" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="姓名" prop="name">
+                    <el-input v-model="ruleForm.name" auto-complete="off"></el-input>
+                </el-form-item>
+                <el-form-item label="角色" prop="role">
+                    <el-select v-model="ruleForm.role" placeholder="请选择">
+                        <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                        </el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="归属" prop="attribution">
+                    <el-select v-model="ruleForm.attribution" placeholder="请选择">
+                        <el-option
+                            :value="ruleForm.attribution" v-show="false">
+                        </el-option>
+                        <ul id="dialogOrgTree" class="ztree"></ul>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="手机号码" prop="phone">
+                    <el-input v-model="ruleForm.phone" auto-complete="off" :disabled="true"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱" prop="email">
+                    <el-input v-model="ruleForm.email" auto-complete="off" :disabled="true"></el-input>
+                </el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+            <el-button type="primary" @click="submitForm('ruleForm')" class="dialog-btn">确 定</el-button>
+            <el-button @click="addUserDialogVisible = false;resetForm('ruleForm')" class="dialog-btn">取 消</el-button>
+            </span>
+        </el-dialog>
+        <!-- 批量添加 -->
+        <el-dialog
+            title="批量添加人员" 
+            :visible.sync="batchAddUserDialogVisible"
+            size="tinys">
+            <div>
+             <el-input type="textarea"  style="float:left;width:460px;height:240px" resize='none'
+             :autosize="{ minRows: 12, maxRows: 12}" placeholder="请输入内容"v-model="textarea">
+             
+             </el-input>
+             <div style="margin-left:480px">
+               <h3>使用说明：</h3>
+               <ul class="contens-wrap">
+                 <li>1.选择一个成员，复制他的角色和授权</li>
+                 <li>2.每行请输入鲁班通行证、一个姓名</li>
+                 <li>3.通行证、姓名、使用空格隔开</li>
+                 <li>4.用户名限制11个字符</li>
+                 <li>示例：布鲁斯123  布鲁斯</li>
+                 <li>5.excel或者txt文本文件中按上面格式准备的帐号信息也可以直接复制到输入框中进行添加</li>
+                 <li>6.可添加数指批量添加的最大个数，通过被克隆的成员授权的客户端节点套餐剩余量最小的套餐计算</li>
+               </ul>
+             </div>
+            </div>
+            
+            <div slot="footer" class="dialog-footer" style="margin-top:25px">
+            <el-button type="primary" @click="batchAddUserDialogVisible= false" class="dialog-btn">确 定</el-button>
+            <el-button  @click="batchAddUserDialogVisible= false" class="dialog-btn">取 消</el-button>
+            </div>
+        </el-dialog>
+        <!-- 周活跃度 -->
+        <el-dialog title="收货地址" :visible.sync="weeklyactivityDialogVisible">
+        <el-table :data="weeklyactivityData">
+        <el-table-column property="date" label="日期" width="150"></el-table-column>
+        <el-table-column property="name" label="姓名" width="200"></el-table-column>
+        <el-table-column property="address" label="地址"></el-table-column>
+        </el-table>
+        </el-dialog>
+
     </div>
 
 </template>
 <script>
 export default {
   data() {
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入通行证"));
+      } else {
+        console.log("后台验证中......");
+        this.ruleForm.phone = "18555524036";
+        this.ruleForm.email = "yunyinyue@163.com";
+        callback();
+      }
+    };
     return {
+      addUserDialogVisible: false,
+      batchAddUserDialogVisible: false,
+      weeklyactivityDialogVisible: false,
+      textarea: "",
       orgValue: "",
+      role: "",
+      ruleForm: {
+        pass: "",
+        name: "",
+        role: "",
+        attribution: "",
+        phone: "",
+        email: ""
+      },
+      rules: {
+        pass: [{ required: true, validator: validatePass, trigger: "blur" }],
+        name: [{ required: false, message: "请输入姓名", trigger: "blur" }],
+        role: [{ required: true, message: "请输入角色", trigger: "blur" }],
+        attribution: [{ required: true, message: "请输入归属", trigger: "blur" }]
+      },
+
       setting: {
         data: {
           simpleData: {
@@ -95,7 +218,17 @@ export default {
           }
         },
         callback: {
-          onClick: this.onClick
+          onClick: this.OrgTreeClick
+        }
+      },
+      DialogOrgSetting: {
+        data: {
+          simpleData: {
+            enable: true
+          }
+        },
+        callback: {
+          onClick: this.DialogorgTreeClick
         }
       },
       zNodes: [
@@ -214,9 +347,40 @@ export default {
           date: "2016-05-03 13:51",
           WAU: "5小时.5次",
           remarks: "超长remarks"
+        },
+        {
+          name: "赵四",
+          pass: "wulijjjj",
+          role: "项目经理",
+          phone: "18075240365",
+          email: "978648117@163.com",
+          date: "2016-05-03 13:51",
+          WAU: "5小时.5次",
+          remarks: "超长remarks"
         }
       ],
-      role: "",
+      weeklyactivityData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄"
+        }
+      ],
       options: [
         {
           value: "选项1",
@@ -244,9 +408,15 @@ export default {
   },
 
   methods: {
-    onClick(event, treeId, treeNode) {
+    OrgTreeClick(event, treeId, treeNode) {
       this.orgValue = treeNode.name;
-      setTimeout(function() {
+      setTimeout(() => {
+        $(".el-select-dropdown__item.selected").click();
+      }, 100);
+    },
+    DialogorgTreeClick(event, treeId, treeNode) {
+      this.ruleForm.attribution = treeNode.name;
+      setTimeout(() => {
         $(".el-select-dropdown__item.selected").click();
       }, 100);
     },
@@ -267,7 +437,36 @@ export default {
     },
     handleCurrentChange(val) {
       console.log(`当前页: ${val}`);
-    }
+    },
+    addUser() {
+      this.addUserDialogVisible = true;
+      setTimeout(() => {
+        $.fn.zTree.init(
+          $("#dialogOrgTree"),
+          this.DialogOrgSetting,
+          this.zNodes
+        );
+      }, 100);
+    },
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("提交中...........");
+          console.log(this.ruleForm);
+          this.addUserDialogVisible = false;
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    },
+    batchAddUser() {
+      this.batchAddUserDialogVisible = true;
+    },
+    deleteUser() {}
   },
   mounted() {
     $.fn.zTree.init($("#orgTree"), this.setting, this.zNodes);
@@ -286,7 +485,7 @@ export default {
   width: 205px;
 }
 
-.el-form-item {
+.el-form_ {
   float: left;
   width: 20%;
   margin-bottom: 0px;
@@ -297,6 +496,7 @@ export default {
   border-top: 1px solid #e6e6e6;
   background-color: #fff;
 }
+
 .textcell {
   height: 50px;
   padding: 0 20px;
@@ -304,6 +504,15 @@ export default {
   overflow: hidden;
   text-overflow: ellipsis;
   white-space: nowrap;
+}
+.contens-wrap > li {
+  padding: 3px 0px;
+}
+.contens-wrap > li:nth-child(1) {
+  color: #e30000;
+}
+.contens-wrap > li:nth-child(5) {
+  color: #e30000;
 }
 </style>
 
