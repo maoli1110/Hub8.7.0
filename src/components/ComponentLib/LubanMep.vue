5cd4ecaabf71fad965e0152a162689005c94303f
small-samll-girl <1441849713@qq.com>
2017-11-13 15:35:46 +0800

    增加三级联动

--- src/components/ComponentLib/LubanMep.vue
+++ src/components/ComponentLib/LubanMep.vue
@@ -1,15 +1,60 @@
 <template>
     <div>
-        <div class="plugins-tips">
-             <img src="http://2t.5068.com/uploads/allimg/161205/68-1612051H502.jpg" alt="" style="width:1620px;height:880px">           
+        <div class="block">
+            <span class="demonstration">默认</span>
+            <el-date-picker
+                v-model="selectDate"
+                type="daterange"
+                placeholder="选择日期范围">
+            </el-date-picker>
+        </div>
+        <div>
+            <input type="text" id="demo" @change="citys"/>
         </div>
     </div>
 </template>
 
 <script>
+    import {getCitys} from '../../api/getData.js'
     export default {
+        data(){
+            return {
+                selectDate:"",
+                cities:[],
+                province:[],
+                counties:[],
+                ruleForm:{
+                    location:"",
+                    countyId:""
+                },
+
+            }
+        },
+        methods:{
+            citys(val){
+                console.log(this.ruleForm)
+            },
+        },
+        mounted(){
+            let vThis = this;
+
+            getCitys().then((data)=>{
+                this.cities = data.data.cities;
+                this.province = data.data.provice;
+                this.counties = data.data.counties;
+
+            })
+            $("#demo").click( function(e) {
+                console.log(vThis.province,'vThis.cities')
+                SelCity(this, e, vThis, vThis.cities, vThis.counties, vThis.province);
+            });
+
+        },
+        created(){
+
+        }
     }
 </script>
 
-<style scoped>  
+<style scoped>
 </style>
