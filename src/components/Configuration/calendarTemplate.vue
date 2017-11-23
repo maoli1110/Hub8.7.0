aa39b13d3412fdbd319cad4be8aa51b02e7843f5
small-samll-girl <1441849713@qq.com>
2017-11-23 13:44:36 +0800

    只读模板更新

--- src/components/Configuration/calendarTemplate.vue
+++ src/components/Configuration/calendarTemplate.vue
@@ -120,13 +120,17 @@
             <template>
                 <el-row class="calendar-main">
                     <el-col :span="24"  class="cal-template" style="margin:0">
-                        <el-col class="template-tips">克隆：24小时日历</el-col>
+                        <el-col class="template-tips" style="line-height:58px;">
+                            <el-col :span="4">24小时时间</el-col>
+                            <el-col :span="6">
+                                <span class="absol span-block" style="width:108px;top:0">日历有效范围：</span><el-input placeholder="显示时间范围" style="margin-left:108px;"></el-input>
+                            </el-col>
+                        </el-col>
                         <vue-scrollbar class="my-scrollbar" ref="VueScrollbar" style="height: 510px;padding:10px;" >
                             <el-col style="min-height:500px;overflow:auto;" class="scroll-me">
                                 <div class="calendar"></div>
                             </el-col>
                         </vue-scrollbar>
-
                     </el-col>
                 </el-row>
             </template>
@@ -526,7 +530,7 @@
                     this.initCalendarSetMethod()
 //                        calendarTemplate = new CalendarSet('2017/01/11', '2017/12/12');
                 }else{
-
+                    this.detailCalendarSetMethod();
                 }
             },
             modifyDataPicker(value){
@@ -574,11 +578,65 @@
 
                 if(column.id=='el-table_1_column_2'){
                     this.lookTemplate  =true;
-                    setTimeout(()=>{
-                        calendarTemplate = new CalendarSet('2017/01/11', '2017/12/12');
-                    })
+                    this.openWindow('show','123')
                 }
             },
+            /* 详情页面日历初始化 */
+            detailinittocopystate() {
+                //修改页面渲染逻辑
+                if(ct.calendarFalg == 0){//复制24小时
+                    if (restDates != null && restDates.length > 0) {// 已经设置过的
+                        var arr = [];
+                        for (var i = 0; i < restDates.length; i++) {
+                            if (new Date(this.value6[0]).getTime() <= new Date(restDates[i]).getTime()&& new Date(restDates[i]).getTime() <= new Date(this.value6[1]).getTime()) {
+                                arr.push(restDates[i])
+                            }
+                        }
+                        // 设置对应非工作日
+                        calendarTemplate.setRestDate(arr);
+                    }
+                }else{//复制标准
+                    var arr = new Array(6, 0);
+                    var chooseDate = this.getRulesDate(arr, this.value6[0], this.value6[1]);
+                    //将所有展示的周六，周日设置为非工作日
+                    calendarTemplate.setRestDate(chooseDate);
+                    if (notWekendRestDates != null && notWekendRestDates.length > 0) {// 已经设置过的
+                        var arr = [];
+                        for (var i = 0; i < notWekendRestDates.length; i++) {
+                            if (new Date(this.value6[1]).getTime() <= new Date(notWekendRestDates[i]).getTime() && new Date(notWekendRestDates[i]).getTime() <= new Date(this.value6[1]).getTime()) {
+                                arr.push(notWekendRestDates[i])
+                            }
+                        }
+                        //将所有展示的周一到周五设置已经设置的非工作日
+                        calendarTemplate.setRestDate(arr);
+                    }
+                    if(isWekendWorkDates!=null && isWekendWorkDates.length > 0){
+                        var arr = [];
+                        for (var i = 0; i < isWekendWorkDates.length; i++) {
+                            if (new Date(this.value6[0]).getTime() <= new Date(isWekendWorkDates[i]).getTime() && new Date(isWekendWorkDates[i]).getTime() <= new Date(this.value6[1]).getTime()) {
+                                arr.push(isWekendWorkDates[i])
+                            }
+                        }
+                        //将所有展示的周六、周日中工作日的设置为工作日
+                        calendarTemplate.setWorkDate(arr);
+                    }
+                }
+                calendarTemplate.readOnly(true);
+            },
+            /* 详情页面日历之外初始化 */
+             detailCalendarSetMethod() {
+
+
+//                if (ct.startDate != null && "" != ct.startDate && ct.endDate != null&& "" != ct.endDate) {
+                    // 创建日历模板
+                    setTimeout(()=>{
+                        calendarTemplate = new CalendarSet(this.value6[0],this.value6[1]);
+                        this.detailinittocopystate();
+                    })
+
+//                }
+            },
+
 
         },
         computed: {
