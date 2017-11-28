d38ec5df34e836c6c1d92d47e25f8ee838927f95
Administrator <caoxiangx@163.com>
2017-11-28 21:56:11 +0800

    订单管理

--- src/api/getData.js
+++ src/api/getData.js
@@ -39,6 +39,8 @@ export const cloudTree = params=> axios.get("../../static/datasource.json");
 export const router = params=> axios.get(`${route}`);
 // GET /order/manage/getEnterpriseServiceList/{currentPage}/{pageSize} 分页获取企业服务列表
 export const getOrderManagementList = params=> axios.get(`${params.url}/order/manage/getEnterpriseServiceList/${params.currentPage}/${params.pageSize}`);
+// POST /order/manage/getBindingList 获取服务与硬件绑定列表
+export const getBindingList=params=>axios.post(`${params.url}/order/manage/getBindingList`)
 /**
  * bim库
  */
@@ -48,15 +50,11 @@ export const getMajorsByCreate = params =>axios.get(`${params.url}rs/bimParamRes
 export const getProjGenre = params =>axios.get(`${params.url}rs/bimParamRest/getProjGenre/${params.isDelete}/${params.packageType}`);
 //Bim筛选条件->专业
 export const getProjType = params =>axios.get(`${params.url}rs/bimParamRest/getProjType/${params.isDelete}/${params.packageType}`);
-
-//bim库列表
-export const getProjects = params =>axios.post(`${params.url}rs/bimRest/getProjects`,params.param)
-//bim创建工程
-export const createProject = params=>axios.post(`${params.url}rs/bimRest/createProject`,params.param);
-//bim创建->授权人员列表
+//bim添加->授权人员列表
 export const getProjAuthUserInfos = params =>axios.get(`${params.url}rs/bimParamRest/getProjAuthUserInfos/${params.deptId}`);
-//bim创建 项目部树结构
-export const zTreeNodes = params =>axios.get(`${params.url}org/admin/nodes`);
+
+//bim创建工程
+export const createProject = params=>axios.post(`${params.url}rs/bimRest/createProject`,params.param)
 /**
  * 云构件库
  * */
