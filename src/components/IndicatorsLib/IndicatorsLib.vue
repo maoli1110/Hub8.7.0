197497b076a3c2e957fd2251c0e758009a60bef8
Administrator <caoxiangx@163.com>
2017-12-05 10:05:21 +0800

    指标库路由修改

--- src/components/IndicatorsLib/IndicatorsLib.vue
+++ src/components/IndicatorsLib/IndicatorsLib.vue
@@ -6,11 +6,11 @@
                 <el-submenu index="/indicatorslib/unchecked/civilEngineering-index">
                     <template slot="title">未审核指标</template>
                     <el-menu-item index="/indicatorslib/unchecked/civilEngineering-index">土建指标（）</el-menu-item>
-                    <el-menu-item index="/indicatorslib/passed/civil-engineering-index">钢筋指标（）</el-menu-item>
+                    <el-menu-item index="/indicatorslib/unchecked/reinforcement-index">钢筋指标（）</el-menu-item>
                 </el-submenu>
                 <el-submenu index="/indicatorslib/passed/civil-engineering-index">
                     <template slot="title">已通过指标</template>
-                    <el-menu-item index="/indicatorslib/unchecked/reinforcement-index">土建指标（）</el-menu-item>
+                    <el-menu-item index="/indicatorslib/passed/civil-engineering-index">土建指标（）</el-menu-item>
                     <el-menu-item index="/indicatorslib/passed/reinforcement-index">钢筋指标（）</el-menu-item>
                 </el-submenu>
                 </el-menu>
