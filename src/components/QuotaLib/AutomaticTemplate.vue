7af8e1e391592e5dee92354af9749150340710d2
fanweiqiu0901 <305322180@qq.com>
2017-12-05 15:33:03 +0800

    定额库 路路由

--- src/components/QuotaLib/AutomaticTemplate.vue
+++ src/components/QuotaLib/AutomaticTemplate.vue
@@ -1,97 +1,6 @@
 <template>
     <div>
-
-        <span class="icon icon-close"></span>
-        <span class="icon icon-initial-branch-company"></span>
-        <span class="icon icon-initial-user"></span>
-        <span class="icon icon-initial-sub-company"></span>
-        <span class="icon icon-initial-project"></span>
-        <span class="icon icon-enterprise-info"></span>
-        <span class="icon icon-personal-info"></span>
-        <span class="icon icon-login-setting"></span>
-        <span class="icon icon-logout-safe"></span>
-        <span class="icon icon-tree-expand"></span>
-        <span class="icon icon-tree-fold"></span>
-        <span class="icon icon-tree-total-expand"></span>
-        <span class="icon icon-tree-total-fold"></span>
-        <span class="icon icon-tree-total-expand-dark"></span>
-        <span class="icon icon-tree-total-fold-dark"></span>
-        <span class="icon icon-export"></span>
-        <span class="icon icon-delete"></span>
-        <span class="icon icon-edit"></span>
-        <span class="icon icon-export-dark"></span>
-        <span class="icon icon-tips-success"></span>
-        <span class="icon icon-tips-error"></span>
-        <span class="icon icon-tips-processing"></span>
-        <span class="icon icon-tips-pending"></span>
-        <span class="icon icon-tips-untreated"></span>
-        <span class="icon icon-file-open"></span>
-        <span class="icon icon-file-fold"></span>
-        <span class="icon icon-invoiceInfo-save"></span>
-        <span class="icon icon-clear"></span>
-        <span class="icon icon-structure-tree-prev"></span>
-        <span class="icon icon-structure-tree-next"></span>
-        <span class="icon icon-structure-tree-update"></span>
-        <span class="icon icon-structure-tree-save"></span>
-        <span class="icon icon-upload"></span>
-        <span class="icon icon-upload-dark"></span>
-        <span class="icon icon-btn-delete"></span>
-        <span class="icon icon-btn-delete-dark"></span>
-        <span class="icon icon-search"></span>
-        <span class="icon icon-authorize"></span>
-        <span class="icon icon-return"></span>
-        <span class="icon icon-signature"></span>
-        <span class="icon icon-unauthorized"></span>
-        <span class="icon icon-send"></span>
-        <span class="icon icon-pass"></span>
-        <span class="icon icon-search-dark"></span>
-        <span class="icon icon-authorize-dark"></span>
-        <span class="icon icon-return-dark"></span>
-        <span class="icon icon-signature-dark"></span>
-        <span class="icon icon-unauthorized-dark"></span>
-        <span class="icon icon-send-dark"></span>
-        <span class="icon icon-pass-dark"></span>
-        <span class="icon icon-info-update"></span>
-        <span class="icon icon-info-update-dark"></span>
-        <span class="icon icon-forbidden"></span>
-        <span class="icon icon-remove"></span>
-        <span class="icon icon-monitor"></span>
-        <span class="icon icon-username"></span>
-        <span class="icon icon-password"></span>
-        <span class="icon icon-selectAll"></span>
-        <span class="icon icon-calendar"></span>
-        <span class="icon icon-forbidden-dark"></span>
-        <span class="icon icon-remove-dark"></span>
-        <span class="icon icon-monitor-dark"></span>
-        <span class="icon icon-authority-management"></span>
-        <span class="icon icon-invoiceInfo-edit"></span>
-        <span class="icon icon-BIM-monitor"></span>
-        <span class="icon icon-BIM-edit"></span>
-        <span class="icon icon-BIM-delete"></span>
-        <span class="icon icon-BIM-authorize"></span>
-        <span class="icon icon-BIM-extract"></span>
-        <span class="icon icon-BIM-unauthorized"></span>
-        <span class="icon icon-BIM-modify"></span>
-        <span class="icon icon-bar-chart"></span>
-        <span class="icon icon-pay-success"></span>
-        <span class="icon icon-modal-alert"></span>
-        <span class="icon icon-modal-success"></span>
-        <span class="icon icon-modal-error"></span>
-        <span class="icon icon-modal-delete"></span>
-        <span class="icon icon-modal-email"></span>
-        <span class="icon icon-no-avatar1"></span>
-        <span class="icon icon-no-avatar2"></span>
-        <span class="icon icon-checkInfo"></span>
-        <span class="icon icon-no-avatar"></span>
-        <span class="icon icon-arrow-top"></span>
-        <span class="icon icon-arrow-bottom"></span>
-        <span class="icon icon-arrow-left"></span>
-        <span class="icon icon-arrow-right"></span>
-        <span class="icon icon-btn-right"></span>
-        <span class="icon icon-alipay"></span>
-        <span class="icon icon-no-avatar3"></span>
-        <span class="icon icon-luban-coin-text"></span>
-
+     111
     </div>
 </template>
 
@@ -101,6 +10,5 @@
 </script>
 
 <style scoped>
-    @import "../../../static/css/icon-menu.css";
 
 </style>
