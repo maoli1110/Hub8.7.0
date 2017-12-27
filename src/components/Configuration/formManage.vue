367368d1978af91102599626adc1057cad435c3d
small-samll-girl <1441849713@qq.com>
2017-12-27 20:05:19 +0800

    清除注释信息

--- src/components/Configuration/formManage.vue
+++ src/components/Configuration/formManage.vue
@@ -1,25 +1,13 @@
 <template>
     <div class="form-change">
         <el-row>
-            <!--    <el-menu  class="el-menu-demo" mode="horizontal" router>
-                    <el-menu-item v-for="menusdata in menusDataFa"  :index="menusdata.routerDump">{{menusdata.name}}</el-menu-item>
-                </el-menu>-->
             <el-col :span="24" class="sub-menus-style">
                 <el-menu  class="el-menu-demo sub-menus" mode="horizontal" router >
                     <el-menu-item v-for="menusdata in menusData"  :index="menusdata.routerDump" :key="menusdata.routerDump">{{menusdata.name}}</el-menu-item>
                 </el-menu>
             </el-col>
         </el-row>
-        <!--     <el-row class="quality-search" >
-                 <el-col :span="16" style="text-align:left">
-                     <el-input  placeholder="请输入内容" icon="search" class="quality-searInput" style="width:30%" v-model="formVal" ></el-input>
-                     <el-icon class="el-icon-circle-cross" v-show="formVal.length>0" @click.native="clearFormSearval"></el-icon>
-                 </el-col>
-             </el-row>-->
-        <!--:default-sort = "{prop: 'date', order: 'descending'}" -->
         <el-table :data="formDataList"  style="width: 100%"  class="form-table"  >
-           <!-- <el-table-column label="序号" width="80" type="index">
-            </el-table-column>-->
             <el-table-column prop="modelName" label="表单类型" >
             </el-table-column>
             <el-table-column prop="updateUser" width="120" label="更新人" >
@@ -30,7 +18,6 @@
             </el-table-column>
             <el-table-column label="操作" width="100" @click.native="addnew">
                 <template slot-scope="scope">
-                    <!--<el-icon class="el-icon-picture" @click.native="changeFormVisible = true"></el-icon>-->
                     <span class="icon-look" @click="showTreeDialog(scope.$index,scope.row)"></span>
                 </template>
 
@@ -67,16 +54,11 @@
                             <el-table-column prop="formName"></el-table-column>
                             <el-table-column width="100">
                                 <template slot-scope="scope"  v-show="zNodes.isForm">
-                                    <!--<el-icon class="el-icon-picture" @click.native="changeFormVisible = true"></el-icon>-->
                                     <span class="icon-eyes" @click="formPriview(scope.$index,scope.row)" ></span>
                                 </template>
 
                             </el-table-column>
                         </el-table>
-                        <!-- <div class="priview-le" v-for="item in zNodes">
-                             <div>{{item.formName}}</div>
-                             <span class="icon-eyes" @click="formPriview" style="margin-top:12px;"></span>
-                         </div>-->
                     </div>
                 </div>
             </div>
@@ -187,8 +169,6 @@
             $.fn.zTree.init($("#formTree"), this.setting, this.zNodes);
             $('.basicSearch input').bind('keyup',this.basicSearch);
             $('.form-dialog-title input').bind('keydown',this.searchformTree);
-//            $("#expandBtn").bind("click",  {type:"expand",operObj:'formTree'}, this.expandNode);
-//            $("#collapseBtn").bind("click", {type:"collapse",operObj:'formTree'}, this.expandNode);
             if(this.$route.path=='/setting/formManage'){
                 $('.sub-menus li').removeClass('is-active');
                 $('.sub-menus li').eq(2).addClass('is-active');
@@ -299,10 +279,6 @@
             },
             getData(){
                 this.getTableList({belong:0,sortType:1})
-                /*   getFormInfosForm({modelId:"3-6000"}).then((res)=>{
-                 this.zNodes = res.data;
-                 $.fn.zTree.init($("#formTree"), this.setting, this.zNodes);
-                 })*/
             },
             formatter(row, column) {
                 return row.address;
@@ -454,17 +430,8 @@
             clearFormSearval(){
                 this.formVal = '';
             },
-            /*     formSearch(){
-             getFormInfos({modelId:'1-1'}).then((res)=>{
-             this.zNodes = res.data;
-             })
-             },*/
             //全部展开和收起
             expandNode(type,operObj) {
-                //var index=layer.load(2);
-//                debugger;
-//                type = e.data.type;
-//                operObj = e.data.operObj;
                 var zTree = $.fn.zTree.getZTreeObj(operObj);
                 var treeNodes = zTree.transformToArray(zTree.getNodes());
                 var flag = true;
